82f59f518e209c6faf37f02feab3b0c71bbc03e5
Egna Polo <info@egnapolo.com>
2023-05-29 20:42:49 +0200

    Initial Commit

---
+++ URLSessionJSONDecoder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  URLSessionJSONDecoder
+//
+//  Created by Egna Lizeth Polo Rubiano on 29/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLSessionJSONDecoder/URLSessionJSONDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionJSONDecoderApp.swift
+//  URLSessionJSONDecoder
+//
+//  Created by Egna Lizeth Polo Rubiano on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionJSONDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
